34088b298ac5e26eec168c110bb2f3583d8beda4
New York Style <war-craft-2010@yandex.ru>
2025-10-26 20:04:01 +0300

    feat: PROJECTTEMPLATE-39 Добавил страницу 404

--- src/app/lib/router/router.tsx
+++ src/app/lib/router/router.tsx
@@ -1,6 +1,6 @@
 import {Navigate, Route, Routes} from 'react-router-dom';
 
-import {Auth, Home, Profile} from '@pages';
+import {Auth, Home, NotFound, Profile} from '@pages';
 import {APP_ROUTES} from '@shared';
 
 import {AppLayout, AuthLayout} from '../../ui';
@@ -23,25 +23,19 @@ export const Router = () => {
       />
 
       <Route
-        path='/'
+        path={APP_ROUTES.ROOT}
         element={
           <ProtectedRoute>
             <AppLayout />
           </ProtectedRoute>
         }
       >
-        <Route
-          index
-          element={<Navigate to={`/${APP_ROUTES.HOME.ROOT}`} replace />}
-        />
+        <Route index element={<Navigate to={APP_ROUTES.HOME.ROOT} replace />} />
         <Route path={APP_ROUTES.HOME.ROOT} element={<Home />} />
         <Route path={APP_ROUTES.USER.ROOT} element={<Profile />} />
       </Route>
 
-      <Route
-        path='*'
-        element={<Navigate to={`/${APP_ROUTES.HOME.ROOT}`} replace />}
-      />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 };
